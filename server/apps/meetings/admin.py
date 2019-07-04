d054fab5a2aa81be109c1a3f31d02577530a627d
007gzs <007gzs@gmail.com>
2019-07-04 10:30:06 +0800

    fix

--- server/apps/meetings/admin.py
+++ server/apps/meetings/admin.py
@@ -10,8 +10,8 @@ from . import models
 utils.site_register(models.Room, editable=False, addable=False, changeable=False,
                     list_display=['create_user', ])
 utils.site_register(models.UserFollowRoom, editable=False, addable=False, changeable=False,
-                    list_display=['room', 'user'], list_filter=[('date', filters.DateFieldListFilter), 'room'])
+                    list_display=['room', 'user'], list_filter=['room'])
 utils.site_register(models.Meeting, editable=False, addable=False, changeable=False,
-                    list_display=['room', 'user'], list_filter=['room', ])
+                    list_display=['room', 'user'], list_filter=[('date', filters.DateFieldListFilter), 'room'])
 utils.site_register(models.MeetingAttendee, editable=False, addable=False, changeable=False,
                     list_display=['meeting', 'user'])
