1a526b7463e7196ede67a942e88ba5ee732d821d
007gzs <007gzs@gmail.com>
2019-07-04 10:24:51 +0800

    admin

--- server/apps/meetings/admin.py
+++ server/apps/meetings/admin.py
@@ -1,6 +1,8 @@
 # encoding: utf-8
 from __future__ import absolute_import, unicode_literals
 
+from apiview import filters
+
 from core import utils
 
 from . import models
@@ -8,8 +10,8 @@ from . import models
 utils.site_register(models.Room, editable=False, addable=False, changeable=False,
                     list_display=['create_user', ])
 utils.site_register(models.UserFollowRoom, editable=False, addable=False, changeable=False,
-                    list_display=['room', 'user'])
+                    list_display=['room', 'user'], list_filter=[('date', filters.DateFieldListFilter), 'room'])
 utils.site_register(models.Meeting, editable=False, addable=False, changeable=False,
-                    list_display=['room', 'user'])
+                    list_display=['room', 'user'], list_filter=['room', ])
 utils.site_register(models.MeetingAttendee, editable=False, addable=False, changeable=False,
                     list_display=['meeting', 'user'])
