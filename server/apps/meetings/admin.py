e74410a9709cc7b5a5e8953263787de018dc53d8
007gzs <007gzs@gmail.com>
2019-07-04 10:12:08 +0800

    admin

--- server/apps/meetings/admin.py
+++ server/apps/meetings/admin.py
@@ -6,4 +6,6 @@ from core import utils
 from . import models
 
 utils.site_register(models.Room, editable=False, addable=False, changeable=False)
+utils.site_register(models.UserFollowRoom, editable=False, addable=False, changeable=False)
 utils.site_register(models.Meeting, editable=False, addable=False, changeable=False)
+utils.site_register(models.MeetingAttendee, editable=False, addable=False, changeable=False)
